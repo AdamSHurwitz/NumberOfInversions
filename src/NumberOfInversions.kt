f7cef133b61d47e378a8c24ea947763bec23e8c3
Adam Hurwitz <adamscotthurwitz@gmail.com>
2017-10-18 22:40:04 -0700

    works with base 2

--- src/NumberOfInversions.kt
+++ src/NumberOfInversions.kt
@@ -2,56 +2,44 @@ import java.io.File
 import java.io.InputStream
 
 object NumberOfInversions {
-
-    var list = arrayOf(4, 3, 2, 1)
-    var numOfInversions = 0
-
+    //todo: Count Inversions With MergeSort
     //runtime: O(n logn)
     //1)base: 1 element array
     //2)recursion: sort and count left and right inversions
     //• left inversion i,j <= n/2, right inversion i,j > n/2, split i <= n/2, j > n/2
     //3)count split inversions when c array contains larger values than b array
     //• when element of 2nd array C gets copied to output D, increment total by num of elements remaining in B
-    @JvmStatic
-    fun main(args: Array<String>) {
+    var numOfInversions = 0
 
-        /*var list = arrayListOf<Int>()
-        val inputStream: InputStream = File("src/integerarray.txt").inputStream()
-        val lineList = mutableListOf<String>()
-        inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it)} }
-        lineList.forEach{list.add(it.toInt())}*/
-
-        var sortedList = mergeSort(list)
-
-        for (i in sortedList) {
-            print(i.toString())
-        }
-
-        println()
-        print("Inversions: " + numOfInversions)
-    }
-
-    private fun mergeSort(list: Array<Int>): Array<Int> {
+    fun mergeSort(list: Array<Int>): Array<Int> {
         if (list.size == 2) {
             return sortedArray(list)
         } else {
-            var firstHalf = mergeSort(list.copyOfRange(0, list.size / 2))
-            var secondHalf = mergeSort(list.copyOfRange(list.size / 2, list.size))
+            var splitArray = getSplitArray(list)
             var i = 0
             var j = 0
             var sortedList = arrayListOf<Int>()
-            while (i < firstHalf.size && j < secondHalf.size) {
-                if (secondHalf[j] < firstHalf[i]) {
-                    //todo: count inversion
-                    sortedList.add(secondHalf[j])
-                    j++
-                } else {
-                    sortedList.add(firstHalf[i])
-                    sortedList.add(secondHalf[j])
+
+            for (x in 0..list.size - 1) {
+                if (i < splitArray.first.size && j < splitArray.second.size) {
+                    if (splitArray.first[i] < splitArray.second[j]) {
+                        sortedList.add(splitArray.first[i])
+                        i++
+                    } else {
+                        sortedList.add(splitArray.second[j])
+                        j++
+                    }
+                }
+                if (j == splitArray.second.size && i < splitArray.first.size) {
+                    sortedList.add(splitArray.first[i])
                     i++
+                }
+                if (i == splitArray.first.size && j < splitArray.second.size) {
+                    sortedList.add(splitArray.second[j])
                     j++
                 }
             }
+
             return sortedList.toTypedArray()
         }
     }
@@ -64,5 +52,22 @@ object NumberOfInversions {
             return list
         }
     }
+
+    private fun getSplitArray(list: Array<Int>): Pair<Array<Int>, Array<Int>> {
+        if (hasBaseOfTwo(list.size)) {
+            return Pair(mergeSort(list.copyOfRange(0, list.size / 2)), mergeSort(list.copyOfRange(list.size / 2, list.size)))
+        } else {
+            return Pair(arrayOf(), arrayOf())
+        }
+    }
+
+    private fun hasBaseOfTwo(size: Int): Boolean {
+        var product = 2
+        while (product < size) {
+            product *= 2
+        }
+        return product == size
+    }
+
 
 }
